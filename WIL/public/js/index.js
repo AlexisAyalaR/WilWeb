aacd078e5613ac83a27a4ef09606dc5a86246fb7
Jean Paul <jeanpaul@MacBook-Pro-de-Jean-2.local>
2017-07-31 20:14:33 -0500

    comencé la función de genera perfil en js

--- WIL/public/js/index.js
+++ WIL/public/js/index.js
@@ -1,2 +1,8 @@
-nosotros = [];
+nosotros = ["Victor", "Emiliano", "Horacio", "Alexis", "Paul", "Jaime", "Andrés"];
+imgs = ["vicky.jpg", "emi.jpg", "horacio.jpg", "alexis.jpg", "paul.jpg", "limon.jpg", "andy.jpg"];
+experiencia = [""];
 num = 7;
+function creaPerfil(num){
+	var card = document.createElement("div");
+	card.setAttribute("class","w3-card");
+}
