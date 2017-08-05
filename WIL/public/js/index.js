9cad38c600ceda7590e74679d25e516ff47ddb46
Jean Paul <jeanpaul@MacBook-Pro-de-Jean-2.local>
2017-08-05 00:10:07 -0500

    cambié el about us

--- WIL/public/js/index.js
+++ WIL/public/js/index.js
@@ -1,6 +1,14 @@
 nombres = ["Victor", "Emiliano", "Horacio", "Alexis", "Paul", "Jaime", "Andrés"];
 imgs = ["../img/vicky.png", "../img/emi.png", "../img/horacio.png", "../img/alexis.png", "../img/paul.png", "../img/limon.png", "../img/andy.png"];
-experiencia = ["Radio ITAM <br> Practicas de verano 2017","Radio ITAM <br> Practicas de verano 2017","Radio ITAM <br> <br>","<br> <br>","<br> <br>","<br> <br>","<br> <br>"];
+cv = 
+["Back-End Master Developer <br> Estudiante de Ingeniería en Computación <br>", 
+"Front-End Master Developer <br> Estudiante de Ingeniería en Computación <br>",
+ "Senior Programer <br> Estudiante de Ingeniería en Telecomunicaciones <br> <br>", 
+ "Back-End Jr. Developer <br> Estudiante de Ingeniería en Computación <br> Estudiante de Matemáticas Aplicadas", 
+ "Front-End Jr. Developer <br> Estudiante de Ingeniería en Computación <br> Estudiante de Ingeniería en Telecomunicaciones", 
+ "Front-End Jr. Developer <br> Estudiante de Ingeniería en Computación <br> <br>", 
+ "Back-End Jr. Developer <br> Estudiante de Ingeniería en Computación <br> Estudiante de Matemáticas Aplicadas"];
+experiencia = ["Radio ITAM <br> Practicas de verano 2017","Radio ITAM <br> Practicas de verano 2017","Radio ITAM <br> <br>","Página del WIL <br> <br>","Página del WIL <br> <br>","<br> <br>","<br> <br>"];
 num = 7;
 function creaPerfil(num){
 	var div = document.createElement("div");
@@ -20,7 +28,7 @@ function creaPerfil(num){
 	var exp = document.createElement("p");
 	div.appendChild(exp);
 	exp.setAttribute("class", "section2__bio");
-	exp.innerHTML = "Proyectos hechos en el WIL: <br>" + experiencia[num];
+	exp.innerHTML = "Perfil: <br>" + cv[num] + "<br> <br> Proyectos hechos en el WIL: <br>" + experiencia[num];
 
 	var sec = document.getElementById("sec");
 	console.log(sec);
