8f23980d6c6d2af5023d5fcd925c8484cd0c7feb
Alexis Ayala <alexis9613@hotmail.com>
2017-08-11 23:18:05 -0500

    formEmail

---
+++ WIL/app/Http/Controllers/mailController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
+use Mail;
+use App\Mail\Proyecto;
+
+class mailController extends Controller
+{
+    public function envia(Request $req){
+
+    	/*
+    	$tipo = $req->input('tipoProyecto');
+    	$desc = $req->input('descripcion');
+    	$telf = $req->input('telefono');
+    	$mail = $req->input('email');
+
+    	Mail::send(new Proyecto($req));
+    	*/
+  	}  
+}
---
+++ WIL/app/Mail/Proyecto.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Http\request;
+
+class Proyecto extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build(Request $req)
+    {
+        return $this->view('mail', [
+            'tipoProy' => $req->tipo,
+            'descrip' => $req->descripcion,
+            'telefono' => $req->telefono,
+            'email' => $req->email
+            ])->to('alexis9613@gmail.com');
+    }
+}
