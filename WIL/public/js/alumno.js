1dd3f2d0458cff8409ca986e46771c4123d50848
JPaulVI <jpaulviruenai@gmail.com>
2017-08-13 01:24:52 -0500

    intento de requests al php intento7

--- WIL/public/js/alumno.js
+++ WIL/public/js/alumno.js
@@ -6,5 +6,5 @@ r.onreadystatechange = function() {
         document.getElementById("request").innerHTML = myObj;
     }
 };
-r.open("POST", "http://localhost/WilWeb/Wil/app/Http/Controllers/loginController.php", true);
+r.open("POST", "http://localhost/WilWeb/Wil/app/Http/Controllers/paul.php", true);
 r.send();
