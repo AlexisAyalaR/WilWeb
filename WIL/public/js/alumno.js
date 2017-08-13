77fec7e45d8b78e3c54b163bdd1f1831c9b6ad71
JPaulVI <jpaulviruenai@gmail.com>
2017-08-13 00:55:48 -0500

    intento de requests al php intento4

--- WIL/public/js/alumno.js
+++ WIL/public/js/alumno.js
@@ -3,7 +3,7 @@ var r = new XMLHttpRequest();
 r.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         myObj = JSON.parse(this.responseText);
-        document.getElementById("request").innerHTML = myObj.name;
+        document.getElementById("request").innerHTML = myObj;
     }
 };
 r.open("POST", "loginController.php", true);
