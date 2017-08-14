79870a0c3af33160568760d44f1d99c4cd5b957b
Alexis Ayala <alexis9613@hotmail.com>
2017-08-14 00:15:52 -0500

    ya

--- WIL/app/Http/Controllers/registraController.php
+++ WIL/app/Http/Controllers/registraController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class loginController extends Controller
+class registraController extends Controller
 {
     public function registraUsuario(Request $req){
 
