b93b2075e9077f0f8b3cd0b6aa2a027047420233
Alexis Ayala <alexis9613@hotmail.com>
2017-08-14 00:34:20 -0500

    ya

--- WIL/app/Http/Controllers/registraController.php
+++ WIL/app/Http/Controllers/registraController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Input;
+use Validator;
 
 class registraController extends Controller
 {
@@ -13,6 +15,29 @@ class registraController extends Controller
     	$passC = $req->input('passC');
     	$nivel = $req->input('nivel');
 
-    	echo $email;
+
+    	$data = Input::except(array('token'));
+
+    	$regla = array(
+    		'email' => 'required|email',
+    		'pass' => 'required|min:6',
+    		'passC' => 'required|same:pass',
+    		'nivel' => 'required'
+    		);
+
+    	$mensaje = array(
+    		'passC.required' => 'Se necesita que las contraseñas sean iguales',
+    		'passC.min' => 'La contraseña tiene que ser mínimo de 6 caracteres',
+    		'passC.same' => 'Tiene que ser la misma contraseña'
+    		);
+
+    	$validador = Validator::make($data, $regla, $mensaje);
+
+    	if($validador->fails()){
+
+    	} else{
+
+    	}
     }
 }
+
