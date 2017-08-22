adf8329956dba299e29949a4a042cb323d95e621
JPaulVI <jpaulviruenai@gmail.com>
2017-08-22 17:42:31 -0500

    prueba

--- WIL/public/js/alumno.js
+++ WIL/public/js/alumno.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function(){
     $.get("/WilWeb/Wil/public/getInfoUsuario", function(data, status){
-    	console.log("data: "+ data + "\nStatus: " + status);
-        alert("Data: " + data + "\nStatus: " + status);
+    	var x = JSON.parse(this);
+    	console.log(x.toString());
     });
 });
