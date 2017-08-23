64550ffc779b05e356ef356d39ff660b775467c9
JPaulVI <jpaulviruenai@gmail.com>
2017-08-22 20:03:45 -0500

    prueba

--- WIL/public/js/personal.js
+++ WIL/public/js/personal.js
@@ -60,6 +60,7 @@ $(document).ready(function(){
     $.get("/WilWeb/Wil/public/getAlumnos", function(data, status){
         console.log(data);
         var x = data.alumnos;
+        console.log(x);
         for(i = 0; i<x.lenght;i++){
             $(#alumno).append("<option value="">"+ x[i] +"</option>");
         }
