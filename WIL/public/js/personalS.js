e75cffbc833ff5f79cd47f3998c47071a34e9e97
JPaulVI <jpaulviruenai@gmail.com>
2017-08-22 20:19:13 -0500

    prueba

---
+++ WIL/public/js/personalS.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $.get("/WilWeb/Wil/public/getUsuarios", function(data, status){
+        console.log(data);
+        var x = data.alumnos;
+        console.log(x);
+        var y;
+        for(i = 0; i<x.length;i++){
+            y = document.createElement("option");
+            y.innerHTML = x[i].nombre;
+            $("#alumnoE").append(y);
+        }
+    });
+});
