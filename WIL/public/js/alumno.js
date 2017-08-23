518ee3b31c899a6371fa0be7efdf20218ab324e1
JPaulVI <jpaulviruenai@gmail.com>
2017-08-23 12:17:50 -0500

    prueba

--- WIL/public/js/alumno.js
+++ WIL/public/js/alumno.js
@@ -32,7 +32,6 @@ $(document).ready(function(){
     	console.log(z);
     	for(i = 0; i<5; i++){
     		var k = z[i];
-    		console.log(k);
     		for(j = 0; i<26; i++){
     			var n = k.horas.substring(j+1,j+2);;
     			console.log(i+""+j);
