623decc3b663aed091a5156520b6840bd90ece39
JPaulVI <jpaulviruenai@gmail.com>
2017-08-23 19:44:18 -0500

    prueba

--- WIL/public/js/personal.js
+++ WIL/public/js/personal.js
@@ -79,6 +79,13 @@ $(document).ready(function(){
 $(document).ready(function(){
     $.get("/WilWeb/Wil/public/getalumnosYUsuario", function(data, status){
         console.log(data);
+        var x = data.alumnos;
+        ver y;
+        for (i = 0; i<x.length; i++) {
+            y = document.createElement("option");
+            y.innerHTML = x[i].nombre;
+            $("#miembros").append(y);
+        }
     });
 });
 
