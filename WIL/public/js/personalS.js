2e25d8c06ab0f7d4d109cebe039e4000c40fbd8f
JPaulVI <jpaulviruenai@gmail.com>
2017-08-27 21:57:47 -0500

    prueba

--- WIL/public/js/personalS.js
+++ WIL/public/js/personalS.js
@@ -91,3 +91,45 @@ $(document).ready(function(){
         }
     });
 });
+
+var arrayLu = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var arrayMa = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var arrayMi = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var arrayJu = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var arrayVi = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+function cambiaValue(cb, n, m){
+    var cb = document.getElementById(cb);
+    a = m - 1;
+    if(cb.checked){
+        switch(n){
+            case 1: arrayLu[a] = 1;
+            break;
+            case 2: arrayMa[a] = 1;
+            break;
+            case 3: arrayMi[a] = 1;
+            break;
+            case 4: arrayJu[a] = 1;
+            break;
+            case 5: arrayVi[a] = 1;
+        }
+    }else{
+        switch(n){
+            case 1: arrayLu[a] = 0;
+            break;
+            case 2: arrayMa[a] = 0;
+            break;
+            case 3: arrayMi[a] = 0;
+            break;
+            case 4: arrayJu[a] = 0;
+            break;
+            case 5: arrayVi[a] = 0;
+        }
+    }
+}
+
+function envia(){
+    var arrayTotal = [arrayLu, arrayMa, arrayMi, arrayJu, arrayVi];
+    //alert(arrayTotal);
+    return document.getElementById("matriz").value = arrayTotal;  
+}
