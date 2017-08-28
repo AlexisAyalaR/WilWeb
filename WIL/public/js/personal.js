e7fb2881dccd8a23e4933d92d87516130c4518a7
JPaulVI <jpaulviruenai@gmail.com>
2017-08-27 19:52:41 -0500

    prueba

--- WIL/public/js/personal.js
+++ WIL/public/js/personal.js
@@ -109,8 +109,9 @@ $("#carga").ready(function(){
     $.get("/WilWeb/Wil/public/getInfoMiembro", function(data, status){
         var a = data;
         console.log(a);
+        var b = data.nombre
         document.getElementById("nombre").innerHTML=a.nombre;
-        console.log(a.nombre);
+        console.log(b);
         var z = data.horario;
         for(i = 0; i<z.length; i++){
             var k = z[i];
